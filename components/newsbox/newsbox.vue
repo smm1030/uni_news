dfc1623206ddec48637c701ec5470b0557a51194
smm1030 <408816223@qq.com>
2023-03-15 23:07:30 +0800

    allfiles

---
+++ components/newsbox/newsbox.vue
@@ -0,0 +1,82 @@
+<template>
+	<view class="newsbox">
+		<view class="pic">
+			<image :src="item.picurl" mode="aspectFill"></image>
+		</view>
+		
+		<view class="text">
+			<view class="title">{{item.title}}</view>
+			<view class="info" v-if="!item.looktime">
+				<view class="font">{{item.author}}</view>
+				<view class="font">{{item.hits}}浏览</view>
+			</view>		
+			<view class="info" v-else>
+				<view>浏览时间{{item.looktime}}</view>
+
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"newsbox",
+		props:{
+			item:{
+				type: Object,
+				default(){
+					return {
+						title: "组件内默认的标题",
+						author: "张三",
+						hits: 668,
+						picture: "../../static/images/nopic.jpg"
+						}
+				}
+			}
+		},
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.newsbox {
+	display: flex;
+	justify-content: space-between;
+	.pic {
+		width: 230rpx;
+		height: 160rpx;
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
+	.text {
+		flex: 1;
+		padding-left: 20rpx;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		.title {
+			font-size: 36 rpx;
+			color: #333;
+			display: -webkit-box;
+			overflow: hidden;
+			text-overflow: ellipsis;
+			-webkit-line-clamp: 2;
+			-webkit-box-orient: vertical;
+		}
+		.info {
+			font-size: 26rpx;
+			color: #999;
+			display: flex;
+			.font {
+				padding-right: 20rpx;
+			}
+		}
+	}
+}
+</style>
---
+++ unpackage/dist/build/mp-weixin/components/newsbox/newsbox.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/newsbox/newsbox"],{"718c":function(t,n,e){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default=void 0;var u={name:"newsbox",props:{item:{type:Object,default:function(){return{title:"组件内默认的标题",author:"张三",hits:668,picture:"../../static/images/nopic.jpg"}}}},data:function(){return{}}};n.default=u},"74e3":function(t,n,e){"use strict";e.d(n,"b",(function(){return u})),e.d(n,"c",(function(){return a})),e.d(n,"a",(function(){}));var u=function(){var t=this.$createElement;this._self._c},a=[]},9173:function(t,n,e){},a50a:function(t,n,e){"use strict";e.r(n);var u=e("718c"),a=e.n(u);for(var c in u)["default"].indexOf(c)<0&&function(t){e.d(n,t,(function(){return u[t]}))}(c);n["default"]=a.a},bc89:function(t,n,e){"use strict";e.r(n);var u=e("74e3"),a=e("a50a");for(var c in a)["default"].indexOf(c)<0&&function(t){e.d(n,t,(function(){return a[t]}))}(c);e("bd43");var i=e("f0c5"),r=Object(i["a"])(a["default"],u["b"],u["c"],!1,null,null,null,!1,u["a"],void 0);n["default"]=r.exports},bd43:function(t,n,e){"use strict";var u=e("9173"),a=e.n(u);a.a}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/newsbox/newsbox-create-component',
+    {
+        'components/newsbox/newsbox-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('543d')['createComponent'](__webpack_require__("bc89"))
+        })
+    },
+    [['components/newsbox/newsbox-create-component']]
+]);
